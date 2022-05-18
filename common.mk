33fdb916f1cb3e551956d2a3552a08f52d379d7a
Namkhai B <me@forkbomb9.ch>
2022-05-18 12:31:52 -0500

    msm8998-common: Potato bringup
    Signed-off-by: Namkhai B <me@forkbomb9.ch>

--- common.mk
+++ common.mk
@@ -38,7 +38,7 @@ TARGET_SUPPORTS_BLUR := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-dot
+    $(LOCAL_PATH)/overlay-posp
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
